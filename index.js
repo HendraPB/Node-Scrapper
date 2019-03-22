5f99ede0105873b9147f9c7841901a66606a21d5
hendrapb <hendrapratama1493@gmail.com>
2019-03-22 15:57:01 +0700

    place scraping result in each category and stop process handler

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+'use strict';
 const cheerio = require('cheerio')
 const Nightmare = require('nightmare')
 const nightmare = Nightmare({ show: true })
 const mongoose = require('mongoose')
+const db = mongoose.connection
 const Promo = require('./model')
-mongoose.connect('mongodb://localhost/promo'); 
+mongoose.connect('mongodb://localhost/promo') 
 const url = 'https://dana.id'
 
 nightmare
@@ -20,14 +22,32 @@ nightmare
     console.error(error)
   })
 
-let getData = html => {
+const getData = html => {
   const $ = cheerio.load(html)
-  $('.promo-tab .promo-card').each((i, val) => {
-    const promo = new Promo({
-      title: $(val).find('h2').text(),
-      date: $(val).find('.date').text(),
-      banner: $(val).find('img').attr('src')
-    });
-    promo.save();
+  db.dropCollection("promos", (error, result) => {
+
+    if(error)
+      console.log("Collection created")
+    else
+      console.log("Refresh collection")
+
+    let counter = 0
+    $('ul#tabs li').each(async (index, value) => {
+      const promo = new Promo({
+        category: $(value).text()
+      })
+      $('#'+$(value).attr('title')).find('.promo-card').each((index, value) => {
+        promo.product.push({
+          title: $(value).find('h2').text(),
+          date: $(value).find('.date').text(),
+          banner: $(value).find('img').attr('src')
+        })
+      })
+      await promo.save()
+      if(++counter == $('ul#tabs li').length){
+        console.log('Promo in each category has been inserted')
+        process.exit(0)
+      }
+    })
   })
 }
--- model.js
+++ model.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PromoSchema = new Schema({
-  title: String,
-  date: String,
-  banner: String
+	category: String,
+	product: {
+	    type: [{
+		  	title: String,
+		  	date: String,
+		  	banner: String
+	  	}]
+  	}
 });
 
 module.exports = mongoose.model('Promo', PromoSchema);
